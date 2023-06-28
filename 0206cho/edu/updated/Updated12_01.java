7516de0c3675997a6eea5af059c85057eec62b30
MinSol <68586044+0206cho@users.noreply.github.com>
2023-06-28 13:27:05 +0900

    06.28 - 힙정렬2

---
+++ 0206cho/edu/updated/Updated12_01.java
@@ -0,0 +1,36 @@
+/*
+ * 힙정렬2 12-01
+ * 
+ * MAX-HEAPIFY : recursive version
+ * 
+ */
+package edu.updated;
+
+public class Updated12_01 {
+	/*
+	 // 노드 i를 루트로 하는 서브트리를 heapify 함
+	 MAX-HEAPIFY(A, i)
+	 {
+	 	if there is no child of A[i]
+	 		return;
+	 	k <- index of the biggest child of i;
+	 	if A[i] >= A[k]
+	 		return;
+	 	exchange A[i] and A[k];
+	 	MAX-HEAPIFY(A, k);
+	 	// root노드에 대한 heapify는 MAX-HEAPIFY(1)을 호출하면 됨
+	 }
+	 
+	 MAX-HEAPIFY(A, i)
+	 {
+	 	while A[i] has a child do
+	 		k <- index of the biggest child of i;
+	 		if A[i] >= A[k]
+	 			return;
+	 		exchange A[i] and A[k];
+	 		i = k;
+	 	end
+	 }
+	 
+	 */
+}
