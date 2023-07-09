a42ecd9da6a8f4e9d122211602e9814daf030955
MinSol <68586044+0206cho@users.noreply.github.com>
2023-07-10 08:53:35 +0900

    07.10 - sorting in linear time: Radix Sort

---
+++ 0206cho/edu/updated/Updated16_01.java
@@ -0,0 +1,27 @@
+/*
+ * sorting in linear time: Radix Sort -  16-01
+ */
+package edu.updated;
+
+public class Updated16_01 {
+	/*
+	 # Radix Sort
+	 - n개의 d자리 정수들
+	 - 가장 낮은 자리수부터 정렬
+	 - 시간 복잡도 O(d(n+k)
+	 	RADIX-SORT(A, d)
+	 		for i <- 1 to d
+	 			do use a stable sort to sort array A on digit i
+	 
+	 # 정렬 알고리즘
+	 Sort Algorithm			T(n)
+	 Bubble sort			O(n^2) 
+	 Insertion sort			O(n^2)
+	 Selection sort			O(n^2)
+	 Quick sort				worst O(n^2) and average O(nlog2n)
+	 Merge sort				O(nlog2n)
+	 Heap sort				O(nlog2n)
+	 Counting sort			O(n+k)
+	 Radix sort				O(d(n+k))
+	 */
+}
